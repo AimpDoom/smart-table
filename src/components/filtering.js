aa1acdf485b61415a147ef949f79f7943ceae25c
Nikita Sidyakov <sidiakov99@gmail.com>
2026-03-21 21:53:45 +0300

    final_commit

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,26 +1,26 @@
 import { createComparison, defaultRules } from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
-const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
   // @todo: #4.1 — заполнить выпадающие списки опциями
   Object.keys(indexes) // Получаем ключи из объекта
-    .forEach((elementName) => {
-      // Перебираем по именам
-      elements[elementName].append(
-        // в каждый элемент добавляем опции
-        ...Object.values(indexes[elementName]) // формируем массив имён, значений опций
-          .map((name) => {
-            const option = document.createElement("option");
-            option.value = name;
-            option.textContent = name;
-            return option;
-            // используйте name как значение и текстовое содержимое
-            // @todo: создать и вернуть тег опции
-          }),
-      );
-    });
+  .forEach((elementName) => {
+    // Перебираем по именам
+    elements[elementName].append(
+      // в каждый элемент добавляем опции
+      ...Object.values(indexes[elementName]) // формируем массив имён, значений опций
+      .map((name) => {
+        const option = document.createElement("option");
+        option.value = name;
+        option.textContent = name;
+        return option;
+        // используйте name как значение и текстовое содержимое
+        // @todo: создать и вернуть тег опции
+      }),
+    );
+  });
+  const compare = createComparison(defaultRules);
 
   return (data, state, action) => {
     // @todo: #4.2 — обработать очистку поля
